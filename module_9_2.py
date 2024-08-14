a7159e2c3791013562d63bbe475cbef35e1d654f
Justmiron007 <raen.96@mail.ru>
2024-08-15 01:30:05 +0500

    Add files via upload

--- module_9_2.py
+++ module_9_2.py
@@ -2,9 +2,7 @@ first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
 first_result = [len(x) for x in first_strings if len(x) > 5]
-second_result = [(first_string, second_string) for first_string in first_strings for second_string in second_strings
-                 if len(first_string) == len(second_string)]
-
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
 third_strings = first_strings + second_strings
 
 third_result = {third_string: len(third_string) for third_string in third_strings if len(third_string) % 2 == 0}
